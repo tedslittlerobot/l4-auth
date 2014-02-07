b776fa5e546d21a48b83e265fb128fa2196aa8cc
Stef Horner <shorner@wearearchitect.com>
2014-02-07 11:44:07 +0000

    basic user repo

--- src/classes/UserRepository.php
+++ src/classes/UserRepository.php
@@ -10,9 +10,12 @@ class UserRepository extends \Repository {
 	 * Dem rulz
 	 * @var array
 	 */
-	protected $rules = [];
-
-	public $messages;
+	protected $rules = [
+		'firstname' => 'required',
+		'lastname' => 'required',
+		'email' => 'email|unique:users',
+		'password' => 'confirmed',
+	];
 
 	public function __construct( User $user )
 	{
@@ -20,9 +23,42 @@ class UserRepository extends \Repository {
 		$this->model = $user;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function fill()
 	{
-		$this->translation->fill( $this->data() );
+		$this->model->fill( $this->data() );
+
+		$this->assignIfExists( [ 'email', 'password' ] );
+	}
+
+	public function create()
+	{
+		if( !$this->validate() )
+		{
+			return false;
+		}
+
+		$this->fill();
+		$this->save();
+
+		return $this->user;
+	}
+
+	public function update( $user )
+	{
+		$this->model = $user;
+
+		if( !$this->validate() )
+		{
+			return false;
+		}
+
+		$this->fill();
+		$this->save();
+
+		return $this->user;
 	}
 
 }
