18b84844292e8c990a5713588427b0602f4f1cfb
Stef Horner <shorner@wearearchitect.com>
2014-02-07 17:29:01 +0000

    set up service provider boot method

--- src/classes/AuthServiceProvider.php
+++ src/classes/AuthServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Tlr\Auth;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider {
@@ -21,10 +22,37 @@ class AuthServiceProvider extends ServiceProvider {
 		$this->commands('Tlr\Auth\UserMakeCommand');
 	}
 
+	/**
+	 * Set up some boot actions
+	 */
 	public function boot()
 	{
-		// @TODO: register some routes
+		$this->package('tlr/l4-auth');
+
+		$this->userEvents( $this->app['events'] );
+
+		$this->routes();
 	}
+
+	/**
+	 * Some User events
+	 * @param  Illuminate\Events\Dispatcher $event
+	 * @todo: move this somewhere more relevant
+	 */
+	public function userEvents( $event )
+	{
+		$event->listen('auth.login', function($user, $remember)
+		{
+			$user->last_login = new Carbon;
+			$user->save();
+		});
+	}
+
+	/**
+	 * Register some routes
+	 * @TODO: register some routes!
+	 */
+	public function routes() { }
 
 	/**
 	 * Get the services provided by the provider.
